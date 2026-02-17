60441b72d7054e5b4032578735cbd5620394a0e3
kshibayama <shibayama.kentaro@gmail.com>
2026-02-17 23:53:17 +0900

    add logging

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from logging import getLogger
+import logging
 
 app = FastAPI()
+logger = getLogger(__name__)
 
 class Ticket(BaseModel):
     name: str
@@ -15,8 +18,10 @@ def read_root():
 
 @app.get("/health")
 def health_check():
+    logger.info('health check')
     return { "ok": True }
 
 @app.post("/tickets")
 def update_ticekt(ticket: Ticket):
+    logger.info('update ticket')
     return ticket
